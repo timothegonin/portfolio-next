ff24172af4909441251580be9bdb976096c46fcf
timotheG <gonin.timothe@gmail.com>
2026-03-17 18:36:34 +0100

    Add education data, display, and details

--- app/formations/[slug]/page.js
+++ app/formations/[slug]/page.js
@@ -1,33 +1,9 @@
 import styles from "./page.module.css";
-
-// Liste des formations (normalement, ça viendrait d'une API ou d'une base de données)
-const formations = {
-"developpeur-concepteur-logiciel": {
-title: "Développeur Concepteur Logiciel",
-description:
-    "Formation en Développement et conception logiciel. Apprenez à créer des applications avec React JS.",
-details: ["RNCP38038 - Niveau 6","Bac +4", "9 mois"],
-image: "/images/developpeur-concepteur-logiciel.jpg",
-},
-"developpeur-front-end": {
-title: "Développeur Front-End",
-description:
-    "Formation en Développement Front-End. Apprenez à créer des sites web responsive et accessibles.",
-details: ["RNCP31114 - Niveau 5", "9 mois"],
-image: "/images/developpeur-front-end.jpg",
-},
-"integrateur-web": {
-title: "Intégrateur Web",
-description:
-    "Dispositif preparatoire d'entrée en école d'informatique.",
-details: ["3 mois"],
-image: "/images/integrateur-web.jpg",
-},
-};
+import formationsData from "@/data/formations.json"
 
 export default async function FormationDetail({ params }) {
 const { slug } = await params;
-const formation = formations[slug];
+const formation = formationsData.find((formation) => formation.slug ===slug);
 
 // Si la formation n'existe pas, afficher un message
 if (!formation) {
@@ -54,7 +30,7 @@ return (
     <div className={styles.details}>
         <h2>Détails</h2>
         <div className={styles.technologies}>
-        {formation.details.map((detail, index) => (
+        {formation.tags.map((detail, index) => (
             <span key={index} className={styles.tech}>
             {detail}
             </span>
@@ -64,4 +40,10 @@ return (
     </div>
 </div>
 );
+}
+
+export function generateStaticParams() {
+    return formationsData.map((formation) => ({
+        slug: formation.slug,
+    }))
 }
