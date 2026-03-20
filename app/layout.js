98e033ce2b343ded85a85110b0f8ce683482f350
timotheG <gonin.timothe@gmail.com>
2026-03-20 22:48:50 +0100

    Optimize fonts

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,20 @@
+import {Inter, Poppins} from 'next/font/google'
 import "./globals.css";
 import Navigation from "@/components/Navigation/Navigation"
 import Footer from "@/components/Footer/Footer"
+
+const inter = Inter({
+  subsets: ['latin'],
+  display: 'swap',
+  variable: '--font-inter',
+})
+
+const poppins = Poppins({
+  weight: ['400', '600', '700'],
+  subsets: ['latin'], 
+  display: 'swap',
+  variable: '--font-poppins',
+})
 
 export const metadata = {
   title: "Portfolio de Timothé GONIN",
@@ -9,8 +23,8 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="fr">
-      <body>
+    <html lang="fr" className={`${inter.varibale} ${poppins.variable}`}>
+      <body className={inter.className}>
         <Navigation/>
         <main>{children}</main>
         <Footer/>
